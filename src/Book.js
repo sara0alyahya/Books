f8de580c503990c3275071021a9b6c64763c60eb
sara0alyahya <sara.a.alyahya@outlook.com>
2019-02-14 08:43:26 +0300

    commit

--- src/Book.js
+++ src/Book.js
@@ -14,13 +14,13 @@ class Book extends React.Component {
         return (
             <div>
                 <h2 onClick={this.hideBook} className={this.state.hide}> ISBN : {this.props.book.isbn}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> TITLE : {this.props.book.title}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> SUBTITLE : {this.props.book.subtitle}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> AUTHOR : {this.props.book.author}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> PUBLISHED : {this.props.book.published}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> PAGES : {this.props.book.pages}</h2>
-                <h2 onClick={this.hideBook} className={this.state.hide}> DESCRIPTION : {this.props.book.description}</h2>
-                <a onClick={this.hideBook} className={this.state.hide} href={this.props.book.website} > {this.props.book.author} </a>
+                <h2 onClick={this.hideBook} > TITLE : {this.props.book.title}</h2>
+                <h2 onClick={this.hideBook} > SUBTITLE : {this.props.book.subtitle}</h2>
+                <h2 onClick={this.hideBook} > AUTHOR : {this.props.book.author}</h2>
+                <h2 onClick={this.hideBook} > PUBLISHED : {this.props.book.published}</h2>
+                <h2 onClick={this.hideBook} > PAGES : {this.props.book.pages}</h2>
+                <h2 onClick={this.hideBook} > DESCRIPTION : {this.props.book.description}</h2>
+                <a onClick={this.hideBook} href={this.props.book.website} > {this.props.book.author} </a>
             </div>
         )
     }
